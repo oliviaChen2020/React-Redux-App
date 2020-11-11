ab999d92190fcc88c3543eed30e1124408ef7cab
Olivia <olivia@macbook-pro.myfiosgateway.com>
2020-11-11 17:20:31 -0500

    Changed Styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React-Redux App: Aren't You Hungry?</h1>
+      <h1>Aren't You Hungry?</h1>
       <Quotes />
     </div>
   );
